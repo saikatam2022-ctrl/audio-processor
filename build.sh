3eee8d1919ca99535a6d7fe80885e86cf29452ae
saikatam2022-ctrl <saikat.am2022@gmail.com>
2025-07-18 16:11:26 +0530

    Update build.sh
    bb,b,,.n

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+# build.sh
 
-# Install system dependencies
-apt-get update && apt-get install -y ffmpeg python3 python3-pip
-
-# Install yt-dlp via pip
-pip3 install yt-dlp
+# Install dependencies
+apt-get update && apt-get install -y ffmpeg
+pip3 install -U yt-dlp
 
 # Install Node dependencies
 npm install
